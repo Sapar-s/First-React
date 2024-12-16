5476af59609076f375b1365716e1c3d7784fb5d6
Sapar-s <smagulsapar52@gmail.com>
2024-12-16 12:41:58 +0800

    fs

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div class="bdy">
+      <h1>Hello World</h1>
+      <h2>Hello Code</h2>
+      <div className="yubn">
+        <button>Click me</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
